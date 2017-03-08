2941c139094323ca8273b7e280f594d5afd19465
ixdu <ix@2du.ru>
2017-03-08 18:29:59 +0500

    [aspxgenerator] initial loading fields from tables

--- tools/convertor.js
+++ tools/convertor.js
@@ -1,4 +1,40 @@
 var fs = require('fs');
+var mysql = require('mysql');
+
+var mysql_config = {
+    host     : 'localhost',
+    user     : 'root',
+    password : '32167',
+    database : 'integralx'
+};
+
+function isEmpty(object) {
+    for (var key in object)
+        if (object.hasOwnProperty(key)) return true;
+    return false;
+}
+
+function getFieldsFromTable(table, cb){
+    var connection = mysql.createConnection(mysql_config);
+
+    connection.connect();
+
+    connection.query('describe vendortypes', function (error, results, fields) {
+	var ind, _fields = {};
+	if (error)
+	    throw error;
+
+	for(ind in results){
+	    if(results[ind].Field != 'CompanyID' ||
+	       results[ind].Field != 'DivisionID' ||
+	       results[ind].Field != 'DepartmentID')
+	    _fields[results[ind].Field] = results[ind];
+	}
+	cb(_fields);
+    });
+
+    connection.end();    
+}
 
 function parse_list(content, file){
     var ind, match, field, re;
@@ -81,8 +117,76 @@ function parse_files(files){
     }
 }
 
-function generate_models(files, menuTitle){
-    var ind, content, find, fields, groups, group, gind;
+function generate_model(file, title, menuTitle, cb){
+    var content, find, fields, groups, group, gind;
+    content = "<?php\nrequire \"./models/gridDataSource.php\";\nclass gridData extends gridDataSource{\nprotected $tableName = \"" + file.tableName + "\"\n;";
+
+    content += "protected $gridFields =" + JSON.stringify(file.gridFields) + ";\n";
+    content += "public $dashboardTitle =\"" + file.label + "\";\n";
+    content += "public $breadCrumbTitle =\"" + file.label + "\";\n";
+    content += "public $idField =\"" + file.keyNames[3] + "\";\n";
+
+    content += "public $editCategories = [\n";
+    groups = file.groups;
+    for(gind in groups){
+	content += "\"" + gind + "\" => [\n";
+
+	group = groups[gind];
+	for(find in group){
+	    content += "\n\"" + find + "\" => [\n" +
+		"\"inputType\" => \"" + group[find].inputType + "\",\n" +
+		"\"defaultValue\" => \"" + group[find].defaultValue + "\"\n" +		    
+		"],"; 
+	}
+	content = content.substring(0,content.length - 1);
+	content += "\n]\n";
+    }
+    content = content.substring(0, content.length - 1);
+    content += "];\n";
+
+    content += "public $columnNames = [\n";
+    fields = file.columnNames;
+    for(find in fields){
+	content += "\n\"" + find + "\" => \"" + fields[find] + "\",";
+	//generate_model(
+    }
+    content = content.substring(0, content.length - 1);
+    content += "];\n";
+    
+    content += "}?>\n";
+    fs.writeFileSync('models/' + title + '.php', content);
+
+    cb("\n[\n" +
+       "\"id\" => \"" + menuTitle + "/" + title + "\",\n" +
+       "\"full\" => $translation->translateLabel('" + file.label + "'),\n" +
+       "\"href\"=> \"" + menuTitle + "/" + title  + "\",\n" +
+       "\"short\" => \"" + (file.label? file.label.substring(0,2) : "") + "\"\n],");
+ }
+
+function process_model(file, title, menuTitle, cb){
+    var group;
+    if(!file.detail || isEmpty(file.groups)){
+	getFieldsFromTable(file.tableName, function(fields){
+	    var ind;
+	    file.groups = {};
+	    group = file.groups["Main"] = {};
+	    for(ind in fields){
+		if(!file.columnNames.hasOwnProperty(ind))
+		    file.columnNames[ind] = ind;
+		group[ind] = {
+		    inputType : "text",
+		    defaultValue : ""
+		};
+	    }
+	    console.log(JSON.stringify(file, null, 3));
+	    generate_model(file, title, menuTitle, cb);
+	});
+    }
+}
+
+function generate_models(files, menuTitle, fcounter){
+    var _fcounter = 0;
+    var ind, content;
     var menuCategories =
 	    "$menuCategories[\"" + menuTitle +  "\"] = [\n" +
 	    "\"type\" => \"submenu\",\n" +
@@ -95,75 +199,43 @@ function generate_models(files, menuTitle){
     for(ind in files){
 	if(!files[ind].list)
 	    continue;
-	content = "<?php\nrequire \"./models/gridDataSource.php\";\nclass gridData extends gridDataSource{\nprotected $tableName = \"" + files[ind].tableName + "\"\n;";
-
-	content += "protected $gridFields =" + JSON.stringify(files[ind].gridFields) + ";\n";
-	content += "public $dashboardTitle =\"" + files[ind].label + "\";\n";
-	content += "public $breadCrumbTitle =\"" + files[ind].label + "\";\n";
-	content += "public $idField =\"" + files[ind].keyNames[3] + "\";\n";
-
-	content += "public $editCategories = [\n";
-	groups = files[ind].groups;
-	for(gind in groups){
-	    content += "\"" + gind + "\" => [\n";
-
-	    group = groups[gind];
-	    for(find in group){
-		content += "\n\"" + find + "\" => [\n" +
-		    "\"inputType\" => \"" + group[find].inputType + "\",\n" +
-		    "\"defaultValue\" => \"" + group[find].defaultValue + "\"\n" +		    
-		    "],"; 
+	process_model(files[ind], ind, menuTitle, function(_menuCategories){
+	    _fcounter++;
+	    menuCategories += _menuCategories;
+	    if(_fcounter == fcounter){
+		menuCategories = menuCategories.substring(0, menuCategories.length - 1);
+		menuCategories += "\n]\n];\n";
+		fs.writeFileSync('models/menuCategories.php', menuCategories);
 	    }
-	    content = content.substring(0,content.length - 1);
-	    content += "\n]\n";
-	}
-	content = content.substring(0, content.length - 1);
-	content += "];\n";
+	});
+   }
+}
 
-	content += "public $columnNames = [\n";
-	fields = files[ind].columnNames;
-	for(find in fields){
-	    content += "\n\"" + find + "\" => \"" + fields[find] + "\","; 
+function main(){
+    var fcounter = 0;
+    if(process.argv.length < 3){
+	console.log('please specify a folder\nusage: convertor.js folder');
+    }else{
+	var dir = fs.readdirSync(process.argv[2]), ind;
+	var files = {}, match;
+	for(ind in dir){
+	    if(match = dir[ind].match(/(.*)List\.aspx$/)){
+		if(files.hasOwnProperty(match[1]))
+		    files[match[1]].list = true;
+		else
+		    files[match[1]] = { list : true };
+		fcounter++;
+	    }else if(match = dir[ind].match(/(.*)Detail\.aspx$/)){
+		if(files.hasOwnProperty(match[1]))
+		    files[match[1]].detail = true;
+		else
+		    files[match[1]] = { detail : true };
+	    }
 	}
-	content = content.substring(0, content.length - 1);
-	content += "];\n";
 	
-	content += "}?>\n";
-	fs.writeFileSync('models/' + ind + '.php', content);
-
-	menuCategories += "\n[\n" +
-	    "\"id\" => \"" + menuTitle + "/" + ind + "\",\n" +
-	    "\"full\" => $translation->translateLabel('" + files[ind].label + "'),\n" +
-	     "\"href\"=> \"" + menuTitle + "/" + ind  + "\",\n" +
-	    "\"short\" => \"" + (files[ind].label? files[ind].label.substring(0,2) : "") + "\"\n],";
-
+	parse_files(files);
+	generate_models(files, process.argv[2].match(/\/(.+)/)[1], fcounter);
     }
-    menuCategories = menuCategories.substring(0, menuCategories.length - 1);
-    menuCategories += "\n]\n];\n";
-    fs.writeFileSync('models/menuCategories.php', menuCategories);
 }
 
-if(process.argv.length < 3){
-    console.log('please specify a folder\nusage: convertor.js folder');
-}else{
-    var dir = fs.readdirSync(process.argv[2]), ind;
-    var files = {}, match;
-    for(ind in dir){
-	if(match = dir[ind].match(/(.*)List\.aspx$/)){
-	    if(files.hasOwnProperty(match[1]))
-		files[match[1]].list = true;
-	    else
-		files[match[1]] = { list : true };
-	}else if(match = dir[ind].match(/(.*)Detail\.aspx$/)){
-	    if(files.hasOwnProperty(match[1]))
-		files[match[1]].detail = true;
-	    else
-		files[match[1]] = { detail : true };
-	}
-    }
-
-    parse_files(files);
-    generate_models(files, process.argv[2].match(/\/(.+)/)[1]);
-    
-    console.log(JSON.stringify(files, null, 3));
-}
+main();
