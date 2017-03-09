5c113cdabbf35f26e1d410f9c8ef2c493f90b43f
ixdu <ix@2du.ru>
2017-03-09 18:39:33 +0500

    grid view is divided into main grid page and included grid, view and edit subpage

---
+++ views/gridView/edit.php
@@ -0,0 +1,103 @@
+<div id="row_editor">
+    <ul class="nav nav-tabs">
+	<?php  
+	//render tabs like Main, Current etc
+	//uses $data(charOfAccounts model) as dictionaries which contains list of tab names
+	foreach($data->editCategories as $key =>$value)
+	    echo "<li role=\"presentation\"". ( $scope->category == $key ? " class=\"active\"" : "")  ."><a href=\"index.php?page=" . $app->page . "&action=" . $scope->action .  "&mode=" . $scope->mode ."&category=" . $key . "&item=" . $scope->item . "\">" . $translation->translateLabel($key) . "</a></li>";
+	?>
+    </ul>
+    <form id="itemData" class="form-material form-horizontal m-t-30">
+	<input type="hidden" name="id" value="<?php echo $scope->item; ?>" />
+	<input type="hidden" name="category" value="<?php echo $scope->category; ?>" />
+	<?php
+	//getting record.
+	$item = $scope->mode == 'edit' ? $data->getEditItem($scope->item, $scope->category) :
+				$data->getNewItem($scope->item, $scope->category);
+	//used as translated field name
+	$translatedFieldName = '';
+	
+	foreach($item as $key =>$value){
+	    $translatedFieldName = $translation->translateLabel(key_exists($key, $data->columnNames) ? $data->columnNames[$key] : $key);
+	    switch($data->editCategories[$scope->category][$key]["inputType"]){
+		    
+		case "text" :
+		    //renders text input with label
+		    echo "<div class=\"form-group\"><label class=\"col-md-6\" for=\"" . $key ."\">" . $translatedFieldName . "</span></label><div class=\"col-md-6\"><input type=\"text\" id=\"". $key ."\" name=\"" .  $key. "\" class=\"form-control\" value=\"" . $value ."\" " .
+			 ( (key_exists("disabledEdit", $data->editCategories[$scope->category][$key]) && $scope->mode == "edit")  || (key_exists("disabledNew", $data->editCategories[$scope->category][$key]) && $scope->mode == "new") ? "readonly" : "")
+			."></div></div>";
+		    break;
+		    
+		case "datepicker" :
+		    //renders text input with label
+		    echo "<div class=\"form-group\"><label class=\"col-md-6\" for=\"" . $key ."\">" . $translatedFieldName . "</span></label><div class=\"col-md-6\"><input type=\"text\" id=\"". $key ."\" name=\"" .  $key. "\" class=\"form-control fdatepicker\" value=\"" . ($value == 'now'? date("m/d/y") : date("m/d/y", strtotime($value))) ."\" " .
+			 ( (key_exists("disabledEdit", $data->editCategories[$scope->category][$key]) && $scope->mode == "edit")  || (key_exists("disabledNew", $data->editCategories[$scope->category][$key]) && $scope->mode == "new") ? "readonly" : "")
+			."></div></div>";
+		    break;
+
+		case "checkbox" :
+		    //renders checkbox input with label
+		    echo "<input type=\"hidden\" name=\"" . $key . "\" value=\"0\"/>";
+		    echo "<div class=\"form-group\"><label class=\"col-md-6\" for=\"" . $key ."\">" . $translatedFieldName . "</span></label><div class=\"col-md-6\"><input class=\"grid-checkbox\" type=\"checkbox\" id=\"". $key ."\" name=\"" .  $key. "\" class=\"form-control\" value=\"1\" " . ($value ? "checked" : "") ." " .
+			 ( (key_exists("disabledEdit", $data->editCategories[$scope->category][$key]) && $scope->mode == "edit") || (key_exists("disabledNew", $data->editCategories[$scope->category][$key]) && $scope->mode == "new") ? "disabled" : "")
+			."></div></div>";
+		    break;
+		    
+		case "dropdown" :
+		    //renders select with available values as dropdowns with label
+		    echo "<div class=\"form-group\"><label class=\"col-sm-6\">" . $translatedFieldName . "</label><div class=\"col-sm-6\"><select class=\"form-control\" name=\"" . $key . "\" id=\"" . $key . "\">";
+		    $method = $data->editCategories[$scope->category][$key]["dataProvider"];
+		    $types = $data->$method();
+		    if($value)
+			echo "<option value=\"" . $value . "\">" . (key_exists($value, $types) ? $types[$value]["title"] : $value) . "</option>";
+		    else
+			echo "<option></option>";
+
+		    foreach($types as $type)
+			if(!$value || $type["value"] != $value)
+			    echo "<option value=\"" . $type["value"] . "\">" . $type["title"] . "</option>";
+		    echo"</select></div></div>";
+		    break;
+	    }
+	}
+	?>
+	<div class="pull-right">
+	    <!--
+		 renders buttons translated Save and Cancel using translation model
+	       -->
+	    <a class="btn btn-info waves-effect waves-light m-r-10" onclick="<?php echo ($scope->mode == "edit" ? "saveItem()" : "createItem()"); ?>">
+		<?php echo $translation->translateLabel("Save"); ?>
+	    </a>
+	    <a class="btn btn-inverse waves-effect waves-light" href="index.php?page=<?php echo $app->page . "&action=" . $scope->action .  ( $scope->mode != "new" ? "&mode=view&category=" . $scope->category . "&item=" . $scope->item : "") ; ?>">
+		<?php echo $translation->translateLabel("Cancel"); ?>
+	    </a>
+	</div>
+    </form>
+    <script>
+     //handler of save button if we in new mode. Just doing XHR request to save data
+     function createItem(){
+	 var itemData = $("#itemData");
+	 $.post("index.php?page=<?php  echo $app->page . "&action=" . $scope->action; ?>&new=true", itemData.serialize(), null, 'json')
+	  .success(function(data) {
+	      console.log('ok');
+	      window.location = "index.php?page=<?php  echo $app->page . "&action=" . $scope->action; ?>";
+	  })
+	  .error(function(err){
+	      console.log('wrong');
+	  });
+     }
+     //handler of save button if we in edit mode. Just doing XHR request to save data
+     function saveItem(){
+	 var itemData = $("#itemData");
+	 $.post("index.php?page=<?php  echo $app->page .  "&action=" . $scope->action; ?>&update=true", itemData.serialize(), null, 'json')
+	  .success(function(data) {
+	      console.log('ok');
+	      window.location = "index.php?page=<?php  echo $app->page .  "&action=" . $scope->action; ?>&mode=view&category=<?php  echo $scope->category . "&item=" . $scope->item ; ?>";
+	  })
+	  .error(function(err){
+	      console.log('wrong');
+	  });
+     }
+    </script>
+</div>
+</div>
---
+++ views/gridView/grid.php
@@ -0,0 +1,59 @@
+<div id="grid_content" class="row">
+    <h3 class="box-title m-b-0"><?php echo $data->dashboardTitle ?></h3>
+    <p class="text-muted m-b-30"><?php echo $data->dashboardTitle ?></p>
+    <div class="table-responsive">
+	<table id="example23" class="table table-striped">
+	    <thead>
+		<tr>
+		    <th></th>
+		    <?php
+		    //getting data for table
+		    $rows = $data->getPage(1);
+		    //renders table column headers using rows data, columnNames(dictionary for corresponding column name to ObjID) and translation model for translation
+		    foreach($rows[0] as $key =>$value)
+			echo "<th>" . $translation->translateLabel($data->columnNames[$key]) . "</th>";
+		    ?>
+		</tr>
+	    </thead>
+	    <tbody>
+		<?php
+		//renders table rows using rows, getted in previous block
+		//also renders buttons like edit, delete of row
+		foreach($rows as $row){
+		    echo "<tr><td>";
+		    if($scope->user["accesspermissions"]["GLEdit"])
+			echo "<a href=\"index.php?page=" . $app->page . "&action=" . $scope->action . "&mode=view&category=Main&item=" . $row[$data->idField] ."\"><span class=\"grid-action-button glyphicon glyphicon-edit\" aria-hidden=\"true\"></span></a>";
+		    if($scope->user["accesspermissions"]["GLDelete"])
+			echo "<span onclick=\"deleteItem('" . $row[$data->idField] . "')\" class=\"grid-action-button glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>";
+		    echo "</td>";
+		    foreach($row as $value)
+			echo "<td>$value</td>";
+		    
+		    echo "</tr>";
+		}
+		?>
+	    </tbody>
+	</table>
+    </div>
+    <div>
+	<a class="btn btn-info waves-effect waves-light m-r-10" href="index.php?page=<?php echo  $app->page; ?>&action=<?php echo $scope->action ?>&mode=new&category=Main">
+	    <?php echo $translation->translateLabel("New"); ?>
+	</a>
+    </div>
+    <script>
+     //hander delete button from rows. Just doing XHR request to delete item and redirect to grid if success
+     function deleteItem(item){
+	 if(confirm("Are you sure?")){
+	     var itemData = $("#itemData");
+	     $.getJSON("index.php?page=<?php  echo $app->page . "&action=" . $scope->action ;  ?>&delete=true&id=" + item)
+	      .success(function(data) {
+		  window.location = "index.php?page=<?php  echo $app->page .  "&action=" . $scope->action; ?>";
+	      })
+	      .error(function(err){
+		  console.log('wrong');
+	      });
+	 }
+     }
+    </script>
+</div>
+
