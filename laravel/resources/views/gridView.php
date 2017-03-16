0cb1d9ac079ac7bfb0ad598a45598dc062707c50
ixdu <ix@2du.ru>
2017-03-16 16:01:15 +0500

    [laravel] implemented ASP style value formatting(used for summs)

--- laravel/resources/views/gridView.php
+++ laravel/resources/views/gridView.php
@@ -73,6 +73,38 @@
 		    </thead>
 		    <tbody>
 			<?php
+			function numberToStr($strin){
+//			    return preg_replace();
+			    return preg_replace('/\B(?=(\d{3})+(?!\d))/', ',', $strin);
+			}
+			function formatField($typeDef, $value){
+			    $match;
+			    if(key_exists("format", $typeDef)){
+				if(preg_match('/(.*)\{\s?0\s?\:\s?(\w)(\d?)\s?\}(.*)/', $typeDef["format"], $match)){
+				    echo $match[1];
+				    switch($match[2]){
+					case 'D' :
+					case 'd' :
+					    break;
+
+					case 'N' :
+					case 'n' :
+					    $afterdot = 2;
+					    if($match[3])
+						$afterdot = $match[2];
+					    if(preg_match('/([-+\d]+)\.(\d+)/', $value, $numberParts)){
+						return numberToStr($numberParts[1]) . '.' . substr($numberParts[2], 0, $afterdot);
+					    }
+					    break;
+				    }
+				    echo $match[4];
+				}
+				else
+				    return $value;
+			    }
+			    else
+				return $value;
+			}
 			//renders table rows using rows, getted in previous block
 			//also renders buttons like edit, delete of row
 			if(count($rows)){
@@ -101,7 +133,7 @@
 						break;
 					    case "text":
 					    case "dropdown":
-						echo $value;
+						echo formatField($data->gridFields[$key], $value);
 						break;
 					}
 					echo "</td>\n";
