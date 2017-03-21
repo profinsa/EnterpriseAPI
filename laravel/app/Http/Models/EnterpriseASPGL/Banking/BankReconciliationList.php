319d9993694d2e5b6d852ba741302c9ea1b0d2e2
ixdu <ix@2du.ru>
2017-03-21 18:24:33 +0500

    [laravel] BankReconciliation and BankReconciliationSummary pages is reworked manually

--- laravel/app/Http/Models/EnterpriseASPGL/Banking/BankReconciliationList.php
+++ laravel/app/Http/Models/EnterpriseASPGL/Banking/BankReconciliationList.php
@@ -1,175 +1,178 @@
 <?php
 namespace App\Models;
- require __DIR__ . "/../../../Models/gridDataSource.php";
+require __DIR__ . "/../../../Models/gridDataSource.php";
+
 class gridData extends gridDataSource{
-protected $tableName = "bankreconciliation";
-public $dashboardTitle ="Bank Reconciliation";
-public $breadCrumbTitle ="Bank Reconciliation";
-public $idField ="BankID";
-public $idFields = ["CompanyID","DivisionID","DepartmentID","BankID"];
-public $gridFields = [
+    protected $tableName = "bankreconciliation";
+    public $dashboardTitle ="Bank Reconciliation";
+    public $breadCrumbTitle ="Bank Reconciliation";
+    public $idField ="BankID";
+    public $idFields = ["CompanyID","DivisionID","DepartmentID","BankID"];
+    public $gridFields = [
+        "BankID" => [
+            "dbType" => "varchar(36)",
+            "inputType" => "text"
+        ],
+        "BankRecStartDate" => [
+            "dbType" => "datetime",
+            "format" => "{0:d}",
+            "inputType" => "datetime"
+        ],
+        "BankRecEndDate" => [
+            "dbType" => "datetime",
+            "format" => "{0:d}",
+            "inputType" => "datetime"
+        ],
+        "GLBankAccount" => [
+            "dbType" => "varchar(36)",
+            "inputType" => "text"
+        ],
+        "BankRecEndingBalance" => [
+            "dbType" => "decimal(19,4)",
+            "addFields" => "CurrencyID",
+            "currencyField" => "CurrencyID",
+            "formatFunction" => "currencyFormat",
+            "inputType" => "text"
+        ]
+    ];
 
-"BankID" => [
-    "dbType" => "varchar(36)",
-    "inputType" => "text"
-],
-"BankRecStartDate" => [
-    "dbType" => "datetime",
-    "format" => "{0:d}",
-    "inputType" => "datetime"
-],
-"BankRecEndDate" => [
-    "dbType" => "datetime",
-    "format" => "{0:d}",
-    "inputType" => "datetime"
-],
-"GLBankAccount" => [
-    "dbType" => "varchar(36)",
-    "inputType" => "text"
-],
-"BankRecEndingBalance" => [
-    "dbType" => "decimal(19,4)",
-    "format" => "{0:n}",
-    "inputType" => "text"
-]
-];
-
-public $editCategories = [
-"Main" => [
-
-"BankID" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecStartDate" => [
-"dbType" => "datetime",
-"inputType" => "datetime",
-"defaultValue" => "now"
-],
-"BankRecEndDate" => [
-"dbType" => "datetime",
-"inputType" => "datetime",
-"defaultValue" => "now"
-],
-"CurrencyID" => [
-"dbType" => "varchar(3)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"CurrencyExchangeRate" => [
-"dbType" => "float",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLBankAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecEndingBalance" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecServiceCharge" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLServiceChargeAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecIntrest" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLInterestAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecOtherCharges" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLOtherChargesAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecAdjustment" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLAdjustmentAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecNotes" => [
-"dbType" => "varchar(255)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"Signature" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"SignaturePassword" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"SupervisorSignature" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"SupervisorPassword" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"ManagerSignature" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"ManagerPassword" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-]
-]];
-public $columnNames = [
-
-"BankID" => "Bank ID",
-"BankRecStartDate" => "Start Date",
-"BankRecEndDate" => "End Date",
-"GLBankAccount" => "GL Account",
-"BankRecEndingBalance" => "Balance",
-"CurrencyID" => "CurrencyID",
-"CurrencyExchangeRate" => "CurrencyExchangeRate",
-"BankRecServiceCharge" => "BankRecServiceCharge",
-"GLServiceChargeAccount" => "GLServiceChargeAccount",
-"BankRecIntrest" => "BankRecIntrest",
-"GLInterestAccount" => "GLInterestAccount",
-"BankRecOtherCharges" => "BankRecOtherCharges",
-"GLOtherChargesAccount" => "GLOtherChargesAccount",
-"BankRecAdjustment" => "BankRecAdjustment",
-"GLAdjustmentAccount" => "GLAdjustmentAccount",
-"BankRecNotes" => "BankRecNotes",
-"Signature" => "Signature",
-"SignaturePassword" => "SignaturePassword",
-"SupervisorSignature" => "SupervisorSignature",
-"SupervisorPassword" => "SupervisorPassword",
-"ManagerSignature" => "ManagerSignature",
-"ManagerPassword" => "ManagerPassword"];
-}?>
+    public $editCategories = [
+        "Main" => [
+            "BankID" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecStartDate" => [
+                "dbType" => "datetime",
+                "inputType" => "datetime",
+                "defaultValue" => "now"
+            ],
+            "BankRecEndDate" => [
+                "dbType" => "datetime",
+                "inputType" => "datetime",
+                "defaultValue" => "now"
+            ],
+            "CurrencyID" => [
+                "dbType" => "varchar(3)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "CurrencyExchangeRate" => [
+                "dbType" => "float",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "GLBankAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecEndingBalance" => [
+                "dbType" => "decimal(19,4)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecServiceCharge" => [
+                "dbType" => "decimal(19,4)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "GLServiceChargeAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecIntrest" => [
+                "dbType" => "decimal(19,4)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "GLInterestAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecOtherCharges" => [
+                "dbType" => "decimal(19,4)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "GLOtherChargesAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecAdjustment" => [
+                "dbType" => "decimal(19,4)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "GLAdjustmentAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecNotes" => [
+                "dbType" => "varchar(255)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "Signature" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "SignaturePassword" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "SupervisorSignature" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "SupervisorPassword" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "ManagerSignature" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "ManagerPassword" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ]
+        ]
+    ];
+    public $columnNames = [
+        "BankID" => "Bank ID",
+        "BankRecStartDate" => "Start Date",
+        "BankRecEndDate" => "End Date",
+        "GLBankAccount" => "GL Account",
+        "BankRecEndingBalance" => "Balance",
+        "CurrencyID" => "CurrencyID",
+        "CurrencyExchangeRate" => "CurrencyExchangeRate",
+        "BankRecServiceCharge" => "BankRecServiceCharge",
+        "GLServiceChargeAccount" => "GLServiceChargeAccount",
+        "BankRecIntrest" => "BankRecIntrest",
+        "GLInterestAccount" => "GLInterestAccount",
+        "BankRecOtherCharges" => "BankRecOtherCharges",
+        "GLOtherChargesAccount" => "GLOtherChargesAccount",
+        "BankRecAdjustment" => "BankRecAdjustment",
+        "GLAdjustmentAccount" => "GLAdjustmentAccount",
+        "BankRecNotes" => "BankRecNotes",
+        "Signature" => "Signature",
+        "SignaturePassword" => "SignaturePassword",
+        "SupervisorSignature" => "SupervisorSignature",
+        "SupervisorPassword" => "SupervisorPassword",
+        "ManagerSignature" => "ManagerSignature",
+        "ManagerPassword" => "ManagerPassword"
+    ];
+}
+?>
--- laravel/app/Http/Models/EnterpriseASPGL/Banking/BankReconciliationSummaryList.php
+++ laravel/app/Http/Models/EnterpriseASPGL/Banking/BankReconciliationSummaryList.php
@@ -1,182 +1,213 @@
 <?php
 namespace App\Models;
- require __DIR__ . "/../../../Models/gridDataSource.php";
+require __DIR__ . "/../../../Models/gridDataSource.php";
+
 class gridData extends gridDataSource{
-protected $tableName = "bankreconciliationsummary";
-public $dashboardTitle ="Reconciliation Summary";
-public $breadCrumbTitle ="Reconciliation Summary";
-public $idField ="BankRecID";
-public $idFields = ["CompanyID","DivisionID","DepartmentID","BankRecID"];
-public $gridFields = [
+    protected $tableName = "bankreconciliationsummary";
+    public $dashboardTitle ="Reconciliation Summary";
+    public $breadCrumbTitle ="Reconciliation Summary";
+    public $idField ="BankRecID";
+    public $idFields = ["CompanyID","DivisionID","DepartmentID","BankRecID"];
+    public $gridFields = [
+        "BankRecID" => [
+            "dbType" => "varchar(36)",
+            "inputType" => "text"
+        ],
+        "GLBankAccount" => [
+            "dbType" => "varchar(36)",
+            "inputType" => "text"
+        ],
+        "BankRecCutoffDate" => [
+            "dbType" => "datetime",
+            "format" => "{0:d}",
+            "inputType" => "datetime"
+        ],
+        "BankRecEndingBalance" => [
+            "dbType" => "decimal(19,4)",
+            "addFields" => "CurrencyID",
+            "currencyField" => "CurrencyID",
+            "formatFunction" => "currencyFormat",
+            "inputType" => "text"
+        ]
+    ];
 
-"BankRecID" => [
-    "dbType" => "varchar(36)",
-    "inputType" => "text"
-],
-"GLBankAccount" => [
-    "dbType" => "varchar(36)",
-    "inputType" => "text"
-],
-"BankRecCutoffDate" => [
-    "dbType" => "datetime",
-    "format" => "{0:d}",
-    "inputType" => "datetime"
-],
-"BankRecEndingBalance" => [
-    "dbType" => "decimal(19,4)",
-    "format" => "{0:n}",
-    "inputType" => "text"
-]
-];
-
-public $editCategories = [
-"Main" => [
-
-"BankRecID" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLBankAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"CurrencyID" => [
-"dbType" => "varchar(3)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"CurrencyExchangeRate" => [
-"dbType" => "float",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecCutoffDate" => [
-"dbType" => "datetime",
-"inputType" => "datetime",
-"defaultValue" => "now"
-],
-"BankRecEndingBalance" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecServiceCharge" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLServiceChargeAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecIntrest" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLInterestAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecAdjustment" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLAdjustmentAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecOtherCharges" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"GLOtherChargesAccount" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecCreditTotal" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecDebitTotal" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecCreditOS" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecDebitOS" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecStartingBalance" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecBookBalance" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecDifference" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecEndingBookBalance" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"BankRecStartingBookBalance" => [
-"dbType" => "decimal(19,4)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"Notes" => [
-"dbType" => "varchar(255)",
-"inputType" => "text",
-"defaultValue" => ""
-]
-]];
-public $columnNames = [
-
-"BankRecID" => "Reconciliation ID",
-"GLBankAccount" => "GL Account",
-"BankRecCutoffDate" => "End Date",
-"BankRecEndingBalance" => "End Balance",
-"CurrencyID" => "CurrencyID",
-"CurrencyExchangeRate" => "CurrencyExchangeRate",
-"BankRecServiceCharge" => "BankRecServiceCharge",
-"GLServiceChargeAccount" => "GLServiceChargeAccount",
-"BankRecIntrest" => "BankRecIntrest",
-"GLInterestAccount" => "GLInterestAccount",
-"BankRecAdjustment" => "BankRecAdjustment",
-"GLAdjustmentAccount" => "GLAdjustmentAccount",
-"BankRecOtherCharges" => "BankRecOtherCharges",
-"GLOtherChargesAccount" => "GLOtherChargesAccount",
-"BankRecCreditTotal" => "BankRecCreditTotal",
-"BankRecDebitTotal" => "BankRecDebitTotal",
-"BankRecCreditOS" => "BankRecCreditOS",
-"BankRecDebitOS" => "BankRecDebitOS",
-"BankRecStartingBalance" => "BankRecStartingBalance",
-"BankRecBookBalance" => "BankRecBookBalance",
-"BankRecDifference" => "BankRecDifference",
-"BankRecEndingBookBalance" => "BankRecEndingBookBalance",
-"BankRecStartingBookBalance" => "BankRecStartingBookBalance",
-"Notes" => "Notes"];
-}?>
+    public $editCategories = [
+        "Main" => [
+            "BankRecID" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "GLBankAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "CurrencyID" => [
+                "dbType" => "varchar(3)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "CurrencyExchangeRate" => [
+                "dbType" => "float",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecCutoffDate" => [
+                "dbType" => "datetime",
+                "inputType" => "datetime",
+                "defaultValue" => "now"
+            ],
+            "BankRecEndingBalance" => [
+                "dbType" => "decimal(19,4)",
+                "inputType" => "text",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "defaultValue" => ""
+            ],
+            "BankRecServiceCharge" => [
+                "dbType" => "decimal(19,4)",
+                "inputType" => "text",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "defaultValue" => ""
+            ],
+            "GLServiceChargeAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecIntrest" => [
+                "dbType" => "decimal(19,4)",
+                "inputType" => "text",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "defaultValue" => ""
+            ],
+            "GLInterestAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecOtherCharges" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "GLOtherChargesAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecAdjustment" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "GLAdjustmentAccount" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecCreditTotal" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecDebitTotal" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecCreditOS" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecDebitOS" => [
+                "dbType" => "decimal(19,4)",
+                "inputType" => "text",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "defaultValue" => ""
+            ],
+            "BankRecStartingBalance" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecBookBalance" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecDifference" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecEndingBookBalance" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "BankRecStartingBookBalance" => [
+                "dbType" => "decimal(19,4)",
+                "currencyField" => "CurrencyID",
+                "formatFunction" => "currencyFormat",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "Notes" => [
+                "dbType" => "varchar(255)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ]
+        ]
+    ];
+    public $columnNames = [
+        "BankRecID" => "Reconciliation ID",
+        "GLBankAccount" => "GL Account",
+        "BankRecCutoffDate" => "End Date",
+        "BankRecEndingBalance" => "End Balance",
+        "CurrencyID" => "Currency ID",
+        "CurrencyExchangeRate" => "Currency Exchange Rate",
+        "BankRecServiceCharge" => "Service Charge",
+        "GLServiceChargeAccount" => "GL Service Charge Account",
+        "BankRecIntrest" => "Intrest",
+        "GLInterestAccount" => "GL Interest Account",
+        "BankRecAdjustment" => "Bank Rec Adjustment",
+        "GLAdjustmentAccount" => "GL Adjustment Account",
+        "BankRecOtherCharges" => "Other Charges",
+        "GLOtherChargesAccount" => "GL Other Charges Account",
+        "BankRecCreditTotal" => "Credit Total",
+        "BankRecDebitTotal" => "Debit Total",
+        "BankRecCreditOS" => "Credit OS",
+        "BankRecDebitOS" => "Debit OS",
+        "BankRecStartingBalance" => "Starting Balance",
+        "BankRecBookBalance" => "Book Balance",
+        "BankRecDifference" => "Difference",
+        "BankRecEndingBookBalance" => "Ending Book Balance",
+        "BankRecStartingBookBalance" => "Starting Book Balance",
+        "Notes" => "Notes"
+    ];
+}
+?>
