d7d90af1577ad69d987fed37d05f215fc1448a0c
ixdu <ix@2du.ru>
2017-04-01 01:13:12 +0500

    newtech version is removed from repository

--- laravel/resources/views/header.php
+++
@@ -1,6 +0,0 @@
-<script src="<?php echo $public_prefix; ?>/dependencies/plugins/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- <script src="<?php echo $public_prefix; ?>/dependencies/assets/bootstrap/dist/js/bootstrap.min.js"></script> -->	
-<script type="text/javascript" src="<?php echo $public_prefix; ?>/assets/js/espo.min.js" data-base-path=""></script>
-<script src="chrome-extension://mclbjdibcpiohnhgkjkbfbnjcafkhani/files/foreground.js"></script>
-<link href="<?php echo $public_prefix; ?>/assets/css/newtechcrm-vertical.css" rel="stylesheet">
-<link href="<?php echo $public_prefix; ?>/assets/css/style.css" rel="stylesheet">
