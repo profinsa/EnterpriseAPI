fb2bfb6ee4916bfa5fff37df6582f6ff541cec2a
ixdu <ix@2du.ru>
2017-05-11 03:17:23 +0500

    changes language selector style to fit size to selected content and making arrov look same in different browsers

--- views/nav/top.php
+++ views/nav/top.php
@@ -20,14 +20,17 @@
 	</ul>
 	<ul class="nav navbar-top-links navbar-right pull-right">
 	    <li>
-		<select class="lang-chooser form-control" onclick="event.stopPropagation();" onchange="changeLanguage(event);">
-		    <option><?php echo $scope->user["language"]; ?></option>
-		    <?php
-		    foreach($translation->languages as $value)
-			if($value != $scope->user["language"])
-			    echo "<option style=\"color:black;\">" . $value . "</option>";
-		    ?>
-		</select>
+		<div class="lang-chooser-container">
+		    <select class="lang-chooser" onchange="changeLanguage(event);" style="padding:0px; position:relative; right:-15px; z-index:5000">
+			<option><?php echo $scope->user["language"]; ?></option>
+			<?php
+			foreach($translation->languages as $value)
+			    if($value != $scope->user["language"])
+				echo "<option style=\"color:black;\">" . $value . "</option>";
+			?>
+		    </select>
+		    <span class="glyphicon glyphicon-chevron-down lang-chooser" style="position:relative; top:-1px; right:0px;font-size:6pt;" aria-hidden="true"></span>
+		</div>
 	    </li>
 	    <li class="right-side-toggle"> <a class="waves-effect waves-light" href="javascript:void(0)"><i class="ti-settings"></i></a></li>
 	    <!-- /.dropdown -->
@@ -38,3 +41,36 @@
     <!-- /.navbar-static-side -->
 </nav>
 <!-- End Top Navigation -->
+
+<script>
+ (function($, window){
+     var arrowWidth = 20;
+
+     $.fn.resizeselect = function(settings) {  
+	 return this.each(function() { 
+
+	     $(this).change(function(){
+		 var $this = $(this);
+
+		 // create test element
+		 var text = $this.find("option:selected").text();
+		 var $test = $("<span>").html(text);
+
+		 // add to body, get width, and get out
+		 $test.appendTo('body');
+		 var width = $test.width();
+		 $test.remove();
+
+		 // set select width
+		 $this.width(width + arrowWidth);
+
+		 // run on start
+	     }).change();
+
+	 });
+     };
+
+     // run by default
+     $("select.lang-chooser").resizeselect();                       
+ })(jQuery, window);
+</script>
