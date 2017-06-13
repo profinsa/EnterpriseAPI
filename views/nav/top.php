7bb213b9b4a517ad96e918e6edbdcada7931b7b8
ixdu <ix@2du.ru>
2017-06-13 15:10:36 +0500

    added more spaces between logout icon and Log Out label. Color changing functions restored and reimplemented.

--- views/nav/top.php
+++ views/nav/top.php
@@ -2,12 +2,21 @@
 <nav class="navbar navbar-default navbar-static-top m-b-0">
     <div class="navbar-header"> 
 	<a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="ti-menu"></i></a>
-	<div class="top-left-part"><a class="logo" href="index.html"><b>
-	    <!--This is dark logo icon-->	<img src="assets/images/stfb-logo.gif" style="width:60px; height:60px" alt="home" class="dark-logo" />
-	    <!--This is light logo icon-->	<img src="assets/images/stfb-logo.gif" alt="home" class="light-logo" /></b><span class="hidden-xs">
-	    <!--This is dark logo text-->	<img src="assets/images/stfb-logo.gif" alt="home" class="dark-logo" />
-	    <!--This is light logo text-->	<img src="assets/images/stfb-logo.gif" alt="home" class="light-logo" /></span></a>
-	    
+	<div class="top-left-part">
+	    <a class="logo" href="index.php">
+		<b>
+		    <!--This is dark logo icon-->
+		    <img src="assets/images/stfb-logo.gif" style="width:60px; height:60px" alt="home" class="dark-logo" />
+		    <!--This is light logo icon-->
+		    <img src="assets/images/stfb-logo.gif" style="width:60px; height:60px"  alt="home" class="light-logo" />
+		</b>
+		<span class="hidden-xs">
+		    <!--This is dark logo text-->
+		    <img src="assets/images/stfb-logo.gif" alt="home" class="dark-logo" />
+		    <!--This is light logo text-->
+		    <img src="assets/images/stfb-logo.gif" alt="home" class="light-logo" />
+		</span>
+	    </a>
 	</div>
 	<ul class="nav navbar-top-links navbar-left hidden-xs">
 	    <li><a href="javascript:void(0)" class="open-close hidden-xs waves-effect waves-light"><i class="icon-arrow-left-circle ti-menu"></i></a></li>
