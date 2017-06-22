b27d943309f8bf1a4e78fb0b8cb6bc43c04b8ff7
tetarenko <eugene.tetarenko@gmail.com>
2017-06-22 13:37:36 +0000

    local libraries

--- views/footer.php
+++ views/footer.php
@@ -21,11 +21,11 @@
 <!-- <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"</script> -->
 
 <!-- start - This is for export functionality only -->
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
-<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
-<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+<script src="dependencies/assets/js/dataTables.buttons.min.js"></script>
+<script src="dependencies/assets/js/buttons.flash.min.js"></script>
+<script src="dependencies/assets/js/jszip.min.js"></script>
+<script src="dependencies/assets/js/pdfmake.min.js"></script>
+<script src="dependencies/assets/js/vfs_fonts.js"></script>
+<script src="dependencies/assets/js/buttons.html5.min.js"></script>
+<script src="dependencies/assets/js/buttons.print.min.js"></script>
 <!-- end - This is for export functionality only -->
--- views/header.php
+++ views/header.php
@@ -13,7 +13,7 @@
   <!-- Bootstrap Core JavaScript -->
   <script src="dependencies/assets/bootstrap/dist/js/bootstrap.min.js"></script>
   <link href="dependencies/plugins/bower_components/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
-  <link href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
+  <link href="dependencies/assets/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
   <!-- Menu CSS -->
   <link href="dependencies/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
   <!-- morris CSS -->
@@ -25,14 +25,14 @@
   <link href="assets/css/style.css" rel="stylesheet">
   <!-- color CSS -->
   <link href="dependencies/assets/css/colors/gray-dark.css" id="theme"  rel="stylesheet">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.min.js"></script>
+  <script src="dependencies/assets/js/bootstrap-datepicker.min.js"></script>
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <script src="dependencies/assets/js/html5shiv.js"></script>
+      <script src="dependencies/assets/js/respond.min.js"></script>
   <![endif]-->
-  <script>
+  <!--<script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -41,7 +41,7 @@
     ga('create', 'UA-19175540-9', 'auto');
     ga('send', 'pageview');
 
-  </script>
+  </script>-->
   <style>
     .dropdown-label {
         padding:10px 0 0 8px;
