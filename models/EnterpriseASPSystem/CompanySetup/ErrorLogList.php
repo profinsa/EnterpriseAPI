efe2d3d100d79adc3bcd67d35da171e737e7061b
ixdu <ix@2du.ru>
2017-08-17 15:30:18 +0500

    error loging is implemented

--- models/EnterpriseASPSystem/CompanySetup/ErrorLogList.php
+++ models/EnterpriseASPSystem/CompanySetup/ErrorLogList.php
@@ -1,107 +1,109 @@
 <?php
 require "./models/gridDataSource.php";
 class gridData extends gridDataSource{
-protected $tableName = "errorlog";
-public $dashboardTitle ="Error Log";
-public $breadCrumbTitle ="Error Log";
-public $idField ="ErrorID";
-public $idFields = ["CompanyID","DivisionID","DepartmentID","ErrorID"];
-public $gridFields = [
+    protected $tableName = "errorlog";
+    public $dashboardTitle ="Error Log";
+    public $breadCrumbTitle ="Error Log";
+    public $idField ="ErrorID";
+    public $idFields = ["CompanyID","DivisionID","DepartmentID","ErrorID"];
+    public $gridFields = [
+        "EmployeeID" => [
+            "dbType" => "varchar(36)",
+            "inputType" => "text"
+        ],
+        "ErrorDate" => [
+            "dbType" => "datetime",
+            "format" => "{0:d}",
+            "inputType" => "datetime"
+        ],
+        "ErrorTime" => [
+            "dbType" => "datetime",
+            "format" => "{0:d}",
+            "inputType" => "datetime"
+        ],
+        "ErrorCode" => [
+            "dbType" => "int(11)",
+            "inputType" => "text"
+        ],
+        "ErrorMessage" => [
+            "dbType" => "varchar(255)",
+            "inputType" => "text"
+        ]
+    ];
 
-"EmployeeID" => [
-    "dbType" => "varchar(36)",
-    "inputType" => "text"
-],
-"ErrorDate" => [
-    "dbType" => "datetime",
-    "format" => "{0:d}",
-    "inputType" => "datetime"
-],
-"ErrorTime" => [
-    "dbType" => "datetime",
-    "format" => "{0:d}",
-    "inputType" => "datetime"
-],
-"ErrorCode" => [
-    "dbType" => "int(11)",
-    "inputType" => "text"
-],
-"ErrorMessage" => [
-    "dbType" => "varchar(255)",
-    "inputType" => "text"
-]
-];
-
-public $editCategories = [
-"Main" => [
-
-"ErrorID" => [
-"dbType" => "int(11)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"EmployeeID" => [
-"dbType" => "varchar(36)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"ErrorDate" => [
-"dbType" => "datetime",
-"inputType" => "datetime",
-"defaultValue" => "now"
-],
-"ErrorTime" => [
-"dbType" => "datetime",
-"inputType" => "datetime",
-"defaultValue" => "now"
-],
-"ScreenName" => [
-"dbType" => "varchar(50)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"ModuleName" => [
-"dbType" => "varchar(50)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"ErrorCode" => [
-"dbType" => "int(11)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"ErrorMessage" => [
-"dbType" => "varchar(255)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"ProcedureName" => [
-"dbType" => "varchar(50)",
-"inputType" => "text",
-"defaultValue" => ""
-],
-"CallTime" => [
-"dbType" => "datetime",
-"inputType" => "datetime",
-"defaultValue" => "now"
-],
-"Error" => [
-"dbType" => "varchar(200)",
-"inputType" => "text",
-"defaultValue" => ""
-]
-]];
-public $columnNames = [
-
-"EmployeeID" => "Employee ID",
-"ErrorDate" => "Error Date",
-"ErrorTime" => "Error Time",
-"ErrorCode" => "Error Code",
-"ErrorMessage" => "Error Message",
-"ErrorID" => "ErrorID",
-"ScreenName" => "ScreenName",
-"ModuleName" => "ModuleName",
-"ProcedureName" => "ProcedureName",
-"CallTime" => "CallTime",
-"Error" => "Error"];
-}?>
+    public $editCategories = [
+        "Main" => [
+            "ErrorID" => [
+                "dbType" => "int(11)",
+                "inputType" => "text",
+                "defaultValue" => "",
+                "disabledEdit" => "true"
+            ],
+            "EmployeeID" => [
+                "dbType" => "varchar(36)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "ErrorDate" => [
+                "dbType" => "datetime",
+                "inputType" => "datetime",
+                "defaultValue" => "now"
+            ],
+            "ErrorTime" => [
+                "dbType" => "datetime",
+                "inputType" => "datetime",
+                "defaultValue" => "now"
+            ],
+            "ScreenName" => [
+                "dbType" => "varchar(50)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "ModuleName" => [
+                "dbType" => "varchar(50)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "ErrorCode" => [
+                "dbType" => "int(11)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "ErrorMessage" => [
+                "dbType" => "varchar(255)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "ProcedureName" => [
+                "dbType" => "varchar(50)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ],
+            "CallTime" => [
+                "dbType" => "datetime",
+                "inputType" => "datetime",
+                "defaultValue" => "now"
+            ],
+            "Error" => [
+                "dbType" => "varchar(200)",
+                "inputType" => "text",
+                "defaultValue" => ""
+            ]
+        ]
+    ];
+    
+    public $columnNames = [
+        "EmployeeID" => "Employee ID",
+        "ErrorDate" => "Error Date",
+        "ErrorTime" => "Error Time",
+        "ErrorCode" => "Error Code",
+        "ErrorMessage" => "Error Message",
+        "ErrorID" => "ErrorID",
+        "ScreenName" => "ScreenName",
+        "ModuleName" => "ModuleName",
+        "ProcedureName" => "ProcedureName",
+        "CallTime" => "CallTime",
+        "Error" => "Error"
+    ];
+}
+?>
