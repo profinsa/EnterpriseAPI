7d17083dfb8c51fee8e2d7becd7a9e30373f336f
ixdu <ix@2du.ru>
2017-08-30 21:04:39 +0500

    Improvements in backlink mechanics. New LedgerTransactions screens ported from NewTechPHP version. Implemented creating new detail items during creating header

--- views/gridView/view.php
+++ views/gridView/view.php
@@ -1,9 +1,14 @@
 <?php
 if(key_exists("back", $_GET)){
-    $backhref = urldecode($_GET["back"]) . "&back={$_GET["back"]}";
-    $back = "&back=" . urlencode($_GET["back"]);
+    if(strpos($_GET["back"], "=" . $ascope["path"] . "&") === false){
+	$backhref = urldecode($_GET["back"]) . "&back=" . urlencode($_GET["back"]);
+	$back = "&back=" . urlencode($_GET["back"]);
+    }else{
+	$backhref = $linksMaker->makeGridLink($ascope["path"]);
+	$back = "&back=" . urlencode($linksMaker->makeGridLink($ascope["path"]));
+    }
 }else{
-    $backhref = "index.php#/?page=grid&action={$ascope["path"]}&mode=grid&category=Main&item=all";
+    $backhref = urlencode("index.php#/?page=grid&action={$ascope["path"]}&mode=grid&category=Main&item=all");
     $back = "";
 }
 function makeId($id){
