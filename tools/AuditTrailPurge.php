797b5b2601e5a43894a2c72d59472693b1ca3bfd
ixdu <ix@2du.ru>
2018-11-27 17:25:51 +0500

    [tools] AuditTrailPurge tool is upgraded to support purge for all companies at once except DINOS, DEFAULT and DEMO

--- tools/AuditTrailPurge.php
+++ tools/AuditTrailPurge.php
@@ -2,11 +2,31 @@
 include './init.php';
 
 $howManyDays = 1; //how many days data need to keep, by default 1 day
-$CompanyID = "DINOS";
-$DivisionID = "DEFAULT";
-$DepartmentID = "DEFAULT";
+$whereBlock = "";
+$whereParts = [];
+if(count($argv) == 1)
+    echo "WRONG : missed arguments";
+else if(count($argv) == 2){
+    if($argv[1] == "ALL"){
+        $companies = DB::select("select CompanyID from companies");
+        foreach($companies as $company)
+            if($company->CompanyID != 'DEFAULT' &&
+               $company->CompanyID != 'Demo' &&
+               $company->CompanyID != 'DINOS')
+                $whereParts[] = "CompanyID='{$company->CompanyID}'";
+        $whereBlock = implode(" OR ", $whereParts);
+    }
+}else if(count($argv) == 4){
+    $CompanyID = $argv[1];
+    $DivisionID = $argv[2];
+    $DepartmentID = $argv[3];
+    $whereBlock = "CompanyID='$CompanyID' AND DivisionID='$DivisionID' AND DepartmentID='$DepartmentID'"; 
+}
 
-file_put_contents("AuditTrail.json", json_encode(DB::select("SELECT * from audittrail where EntryDate < NOW() - INTERVAL $howManyDays DAY AND CompanyID='$CompanyID' AND DivisionID='$DivisionID' AND DepartmentID='$DepartmentID'", array()), JSON_PRETTY_PRINT), FILE_APPEND);
-DB::delete("delete from audittrail where EntryDate < NOW() - INTERVAL $howManyDays DAY AND CompanyID='$CompanyID' AND DivisionID='$DivisionID' AND DepartmentID='$DepartmentID'");
+echo $whereBlock;
+//echo count($argv)  . json_encode($argv, JSON_PRETTY_PRINT);
+
+file_put_contents("AuditTrail.json", json_encode(DB::select("SELECT * from audittrail where EntryDate < NOW() - INTERVAL $howManyDays DAY AND $whereBlock", array()), JSON_PRETTY_PRINT), FILE_APPEND);
+DB::delete("delete from audittrail where EntryDate < NOW() - INTERVAL $howManyDays DAY AND $whereBlock");
 
 ?>
