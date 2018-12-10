53273c59ff2669da678cb9d57b12ba03c3dd4312
ixdu <ix@2du.ru>
2018-12-10 16:51:24 +0500

    added actions to RMA Approve screen

--- models/EnterpriseASPAR/RMA/RMAHeaderList.php
+++ models/EnterpriseASPAR/RMA/RMAHeaderList.php
@@ -1383,9 +1383,11 @@ class RMAHeaderApproveList extends RMAHeaderList{
         }
 
         if($success)
-            header('Content-Type: application/json');
-        else
-            return response("failed", 400)->header('Content-Type', 'text/plain');
+            echo "ok";
+        else {
+            http_response_code(400);
+            echo "failed";            
+        }
     }
     
     public function ApproveAll(){
@@ -1396,8 +1398,10 @@ class RMAHeaderApproveList extends RMAHeaderList{
         $result = DB::select('select @SWP_RET_VALUE as SWP_RET_VALUE');
         if($result[0]->SWP_RET_VALUE > -1)
             echo $result[0]->SWP_RET_VALUE;
-        else
-            return response($result[0]->SWP_RET_VALUE, 400)->header('Content-Type', 'text/plain');
+        else {
+            http_response_code(400);
+            echo $result[0]->SWP_RET_VALUE;
+        }
     }
 }
 
