e695ebef1b2d03102a1e4716b58e30b5bb1aa541
DEV1111111 <dev1@nitrosdata.com>
2019-01-11 18:42:30 +0500

    add companylist logo fields

--- sql/updates/update_11_01_2019.sql
+++ sql/updates/update_11_01_2019.sql
@@ -1,3 +1,5 @@
 alter table inventoryitems add column Picture NvarChar(80) after PictureURL;
 alter table inventoryfamilies add column FamilyPicture NvarChar(80) after FamilyPictureURL;
 alter table inventorycategories add column CategoryPicture NvarChar(80) after CategoryPictureURL;
+alter table companies add column MediumLogo NvarChar(255) after Logo;
+alter table companies add column SmallLogo NvarChar(255) after MediumLogo;
