7d812d9ce2e9fa9da4bae1dc85d418fa55da783c
ixdu <ix@2du.ru>
2019-06-20 22:38:57 +0500

    format functions for reports now placed on modesl/reports/format, all financials reports uses that file

--- models/reports/financials/gaap/CashFlow.php
+++ models/reports/financials/gaap/CashFlow.php
@@ -26,46 +26,7 @@
   Last Modified by: Nikita Zaharov
 */
 
-function numberToStr($strin){
-    return preg_replace('/\B(?=(\d{3})+(?!\d))/', ',', $strin);
-}
-
-function formatCurrency($value){
-    $afterdot = 2;
-    //    if($cflag
-    if(preg_match('/([-+\d]+)\.(\d+)/', $value, $numberParts))
-        return numberToStr($numberParts[1]) . '.' . substr($numberParts[2], 0, $afterdot);
-    else
-        return numberToStr($value) . ".00";
-
-}
-
-function gridFormatFields($stmt, $result){
-    if(count($result)){
-        $meta = [];
-        $mcounter = 0;
-        foreach($result[0] as $key=>$value){
-            $meta[$key] = $stmt->getColumnMeta($mcounter++);
-        }
-        
-        //formatting data
-        foreach($result as $rkey=>$row){
-            foreach($row as $key=>$value){
-                $result[$rkey][$key . "Original"] = $value;
-                if($meta[$key]["native_type"] == "NEWDECIMAL" || $meta[$key]["native_type"] == "DECIMAL" || $meta[$key]["native_type"] == "LONGLONG" || preg_match('/^\d+\.\d+/', $value)){
-                    $afterdot = 2;
-                    if($meta[$key]["native_type"] == "LONGLONG")
-                        $result[$rkey][$key] = numberToStr($value) . ".00";
-                    else if(preg_match('/([-+\d]+)\.(\d+)/', $value, $numberParts))
-                        $result[$rkey][$key] = numberToStr($numberParts[1]) . '.' . substr($numberParts[2], 0, $afterdot);
-                }else if($meta[$key]["native_type"] == "TIMESTAMP" || $meta[$key]["native_type"] == "DATETIME")
-                    if($value != "")
-                        $result[$rkey][$key] = date("m/d/y", strtotime($value));
-            }
-        }
-    }
-    return $result;
-}
+require_once "./models/reports/format.php";
 
 class financialsReportData{
     public $title = "DIRECT CASH FLOW STATEMENT";
@@ -135,7 +96,7 @@ class financialsReportData{
         $data = [];
 
         //       echo "RptGLCashFlow" . $typesToProc[$this->type] . ( $_GET["itype"] == "Standard" ? "" : $_GET["itype"]) . "Drills(";
-        echo json_encode($result[0]);
+        //echo json_encode($result[0]);
         $itype = $_GET["itype"];
         
         $data["OperatingDebitsTotal"] = 0;
--- models/reports/financials/gaap/IncomeStatement.php
+++ models/reports/financials/gaap/IncomeStatement.php
@@ -26,46 +26,7 @@
   Last Modified by: Nikita Zaharov
 */
 
-function numberToStr($strin){
-    return preg_replace('/\B(?=(\d{3})+(?!\d))/', ',', $strin);
-}
-
-function formatCurrency($value){
-    $afterdot = 2;
-    if(preg_match('/([-+\d]+)\.(\d+)/', $value, $numberParts))
-        return numberToStr($numberParts[1]) . '.' . substr($numberParts[2], 0, $afterdot);
-    else
-        return numberToStr($value) . ".00";
-
-}
-
-function gridFormatFields($stmt, $result){
-    if(count($result)){
-        $meta = [];
-        $mcounter = 0;
-        foreach($result[0] as $key=>$value){
-            $meta[$key] = $stmt->getColumnMeta($mcounter++);
-        }
-        
-        //formatting data
-        foreach($result as $rkey=>$row){
-            foreach($row as $key=>$value){
-                $result[$rkey][$key . "Original"] = $value;
-                if($meta[$key]["native_type"] == "NEWDECIMAL" || $meta[$key]["native_type"] == "DECIMAL" || $meta[$key]["native_type"] == "LONGLONG"){
-                    $afterdot = 2;
-                    if($meta[$key]["native_type"] == "LONGLONG")
-                        $result[$rkey][$key] = numberToStr($value) . ".00";
-                    else if(preg_match('/([-+\d]+)\.(\d+)/', $value, $numberParts))
-                        $result[$rkey][$key] = numberToStr($numberParts[1]) . '.' . substr($numberParts[2], 0, $afterdot);
-                }
-                if($meta[$key]["native_type"] == "TIMESTAMP" || $meta[$key]["native_type"] == "DATETIME")
-                    if($value != "")
-                        $result[$rkey][$key] = date("m/d/y", strtotime($value));
-            }
-        }
-    }
-    return $result;
-}
+require_once "./models/reports/format.php";
 
 class financialsReportData{
     public $title = "INCOME STATEMENT";
