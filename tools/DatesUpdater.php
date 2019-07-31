d92d028e4208763f132aab1e76a7b385d4c0dc7a
ixdu <ix@2du.ru>
2019-07-31 16:12:34 +0500

    implemented datetime and timestamp fields getting from all tables in DatesUpdater script

--- tools/DatesUpdater.php
+++ tools/DatesUpdater.php
@@ -1,29 +1,73 @@
 <?php
 include './init.php';
 
-//$companiesStr = "Test";
-//$companiesStr = '';
-//$companies = explode(",",$companiesStr);
-$companies = [];
+$columnIgnoreList = [
+    "LockTS",
+    "LoginDateTime",
+    "EntryDate",
+    "EntryTime",
+    "SystemDate",
+    "CreditCardExpDate",
+    "CustomerBornDate",
+    "HireDate",
+    "Birthday",
+    "LastSessionUpdateTime"
+];
+$tableIgnoreList = [
+    "companies",
+    "audittrail",
+    "audittraishistory",
+    "currencytypes",
+    "currencytypeshistory",
+    "errorlog",
+    "ledgerchartofaccountsprioryears",
+    "ledgerstoredchartofaccounts"
+];
+
+$tablesColumns = [];
 foreach($tables as $tableName){
     $desc = DB::select("describe $tableName", array());
     $keys = 0;
     foreach($desc as $column)
         if($column->Field == "CompanyID" || $column->Field == "DivisionID" || $column->Field == "DepartmentID")
             $keys++;
-    if($keys == 3)
+    if($keys == 3 && !in_array($tableName, $tableIgnoreList))
         $tablesColumns[$tableName] = $desc;
+    else
+        echo "Ignoring table $tableName\n";
 }
 
 $fields = [];
 //echo json_encode($tablesColumns["orderheader"], JSON_PRETTY_PRINT);
-foreach($tablesColumns as $name=>$desc)
+foreach($tablesColumns as $name=>$desc){
     foreach($desc as $columnDesc)
         if($columnDesc->Type == "datetime" || $columnDesc->Type == "timestamp"){
             $columnDesc->tableName = $name;
-            $fields[$columnDesc->Field] = $columnDesc;
+            if(!in_array($columnDesc->Field, $columnIgnoreList))
+                $fields[$columnDesc->Field] = $columnDesc;
+            //            else
+            //  echo "Ignoring column {$columnDesc->Field}\n";
+            //$fields[] = $columnDesc;
         }
-echo count($fields);
+}
+file_put_contents("fields.json", json_encode($fields, JSON_PRETTY_PRINT));
+
+foreach($tablesColumns as $name=>$desc){
+    $columns = [];
+    foreach($desc as $columnDesc)
+        if(($columnDesc->Type == "datetime" || $columnDesc->Type == "timestamp") &&
+           !in_array($columnDesc->Field, $columnIgnoreList))
+            $columns[] = $columnDesc->Field;
+    if(count($columns)){
+        $result = DB::select("select " . implode(",", $columns) . " from $name");
+        if(count($result))
+            echo "processing $name\n";
+        else
+            echo "table $name is empty\n";
+    }
+}
+//echo count($tablesColumns);
+//echo count($fields);
 //echo json_encode($fields, JSON_PRETTY_PRINT);
 /*
 $whereBlock = "";
