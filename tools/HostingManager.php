6dd24a162ffad8d07eb6405a7e9550c97a3e0915
Nikita Zaharov <ix@2du.ru>
2019-09-03 10:43:22 -0700

    [Admin] implemented support for Windows in HostingManager

--- tools/HostingManager.php
+++ tools/HostingManager.php
@@ -5,9 +5,11 @@ $GLOBALS["DBDONTCACHE"] = true;
 include './init.php';
 
 $dbuser = "root";
-$dbpass = "32167";
+//$dbpass = "32167";
+$dbpass = "mysqlroot";
 
-$mysqlcmd = substr(php_uname(), 0, 7) == "Windows" ? "\"C:\Program Files\MySQL\MySQL Server 5.5\bin\mysql.exe\" \"--defaults-file=C:\Program Files\MySQL\MySQL Server 5.5\my.ini\" \"-u$dbuser\" \"-p$dbpass\"" : "mysql -u $dbuser -p$dbpass";
+$mysqlcmd = substr(php_uname(), 0, 7) == "Windows" ? "\"C:\Program Files\MySQL\MySQL Server 5.5\bin\mysql.exe\" \"--defaults-file=C:\Program Files\MySQL\MySQL Server 5.5\my.ini\" -u$dbuser -p$dbpass" : "mysql -u $dbuser -p$dbpass";
+$mysqlcmd1 = substr(php_uname(), 0, 7) == "Windows" ? "\"C:\Program Files\MySQL\MySQL Server 5.5\bin\mysql.exe --defaults-file=C:\Program Files\MySQL\MySQL Server 5.5\my.ini -u$dbuser -p$dbpass" : "\"mysql -u $dbuser -p$dbpass";
 
 
 function generateRandomString($length = 10) {
@@ -29,11 +31,13 @@ while(1){
     if(count($installations) < 3){
         $dbname = "db" . generateRandomString(20);
     
-        $cmd1 = "echo \"CREATE DATABASE $dbname; GRANT ALL PRIVILEGES ON $dbname.* to 'enterprise'@'localhost'\" | $mysqlcmd";
-        system($cmd1, $retval);
+	    $cmd1 = "echo CREATE DATABASE $dbname; GRANT ALL PRIVILEGES ON {$dbname}.* to 'enterprise'@'localhost' | $mysqlcmd";
+		echo $cmd1 . "\n";
+        exec($cmd1, $retval);
         //$cmd2 = "$mysqlcmd $dbname < ../database/updates/update_2019_08_30.sql";
         $cmd2 = "$mysqlcmd $dbname < ../database/backups/cleanenterprise.sql";
-        execInBackground($cmd2);
+		echo $cmd2 . "\n";
+        exec($cmd2);
         DB::INSERT("INSERT INTO appinstallations (CustomerID, ConfigName, InstallationName, InstallationDate, ExpirationDate, Active, Clean) values('DEFAULT', ?, ?, now(), now(), 0, 1)", [$dbname, $dbname]);
     }
 }
