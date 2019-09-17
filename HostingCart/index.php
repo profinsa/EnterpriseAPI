cde23818f0b828331db8cce2fe474406f5a6a14a
ixdu <ix@2du.ru>
2019-09-17 19:22:05 +0500

    [Admin] small fix for Hosting Cart index to properly using GET parameters

--- HostingCart/index.php
+++ HostingCart/index.php
@@ -1,3 +1,7 @@
 <?php
-header("Location: /EnterpriseX/Cart/index.php?config=AppShop" . (count($_GET) ? "&" . implode("&", $_GET) : ""));
+$params = [];
+if(count($_GET))
+    foreach($_GET as $key=>$value)
+        $params[] = "$key=$value";
+header("Location: /EnterpriseX/Cart/index.php?config=AppShop" . (count($_GET) ? "&" . implode("&", $params) : ""));
 ?>
