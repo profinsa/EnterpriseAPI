437d554fdbc5e013b5cb81dc0dabe9b6ad89fc43
ixdu <ix@2du.ru>
2019-11-05 23:41:07 +0500

    implemented GLAccountColumnChanger script for chaning type of all GL Account columns in database

---
+++ tools/GLAccountColumnChanger.php
@@ -0,0 +1,72 @@
+<?php
+//$GLOBALS["configName"] = "Admin";
+$GLOBALS["DBDONTCACHE"] = true;
+$dbuser = "root";
+$dbpass = "32167";
+//$dbpass = "mysqlroot";
+
+include './init.php';
+
+$columnIgnoreList = [];
+
+$tableIgnoreList = [
+    "companies",
+    "audittrail",
+    "audittraishistory",
+    "currencytypes",
+    "currencytypeshistory",
+    "errorlog",
+    "ledgerchartofaccountsprioryears",
+    "ledgerstoredchartofaccounts",
+    "inventoryledger"
+];
+
+$tablesColumns = [];
+//echo json_encode($tables, JSON_PRETTY_PRINT);
+
+foreach($tables as $tableName){
+    $desc = DB::select("describe $tableName", array());
+    $keys = 0;
+    foreach($desc as $column)
+        if($column->Field == "CompanyID" || $column->Field == "DivisionID" || $column->Field == "DepartmentID")
+            $keys++;
+    if($keys == 3 && !in_array($tableName, $tableIgnoreList))
+        $tablesColumns[$tableName] = $desc;
+    else
+        echo "Ignoring table $tableName\n";
+}
+
+$fields = [];
+//echo json_encode($tablesColumns["orderheader"], JSON_PRETTY_PRINT);
+
+foreach($tablesColumns as $name=>$desc){
+    foreach($desc as $columnDesc){
+        if(preg_match('/GL.*Account[$\d]*/', $columnDesc->Field) && preg_match('/varchar/', $columnDesc->Type)){
+            $columnDesc->tableName = $name;
+            if(!in_array($columnDesc->Field, $columnIgnoreList))
+                $fields[$name . '@' . $columnDesc->Field] = $columnDesc;
+            // else
+            //  echo "Ignoring column {$columnDesc->Field}\n";
+            //$fields[] = $columnDesc;
+        }
+    }
+}
+
+file_put_contents("fields.json", json_encode($fields, JSON_PRETTY_PRINT));
+
+$sqlQueries = [];
+foreach($fields as $name=>$desc){
+    $sqlQueries[] = "alter table {$desc->tableName} MODIFY COLUMN {$desc->Field} NVARCHAR(255);";
+}
+
+//echo $sqlQueries;
+
+foreach($sqlQueries as $query){
+    $mysqlcmd = substr(php_uname(), 0, 7) == "Windows" ? "\"C:\Program Files\MySQL\MySQL Server 5.5\bin\mysql.exe\" \"--defaults-file=C:\Program Files\MySQL\MySQL Server 5.5\my.ini\" -u$dbuser -p$dbpass" : "mysql -u $dbuser -p$dbpass";
+    $mysqlcmd1 = substr(php_uname(), 0, 7) == "Windows" ? "\"C:\Program Files\MySQL\MySQL Server 5.5\bin\mysql.exe --defaults-file=C:\Program Files\MySQL\MySQL Server 5.5\my.ini -u$dbuser -p$dbpass" : "\"mysql -u $dbuser -p$dbpass";
+
+    $cmd1 = "echo \"$query\" | $mysqlcmd enterprise -f";
+    echo $cmd1 . "\n";
+    exec($cmd1, $retval);
+}
+?>
