e39df47ef9695a3399f1371bc35dd79ed3c01c51
ixdu <ix@2du.ru>
2020-04-21 22:46:13 +0500

    [mssql] implemented cross-base DB method for getting information about stored Function or stored Procedure

--- init.php
+++ init.php
@@ -50,6 +50,7 @@ class DB{
             return $columns;
         }            
     }
+    
     public static function statement($query, $args = false){
         if(key_exists("user", $_SESSION) && key_exists("EmployeeID", $_SESSION["user"])){
             if($GLOBALS["config"]["db_type"] == "mysql")
@@ -58,6 +59,7 @@ class DB{
         }
         return $GLOBALS["DB"]::statement($query, $args ? $args : array());
     }
+    
     public static function select($query, $args = false){
         if(key_exists("user", $_SESSION) && key_exists("EmployeeID", $_SESSION["user"])){
             if($GLOBALS["config"]["db_type"] == "mysql")
@@ -93,6 +95,7 @@ class DB{
         //        file_put_contents("sqllog", $query . "\n", FILE_APPEND);
         return $result;
     }
+    
     public static function update($query, $args = false){
         if(key_exists("user", $_SESSION) && key_exists("EmployeeID", $_SESSION["user"])){
             if($GLOBALS["config"]["db_type"] == "mysql")
@@ -101,6 +104,7 @@ class DB{
         }
         return $GLOBALS["DB"]::update($query, $args ? $args : array());
     }
+    
     public static function insert($query, $args = false){
         if(key_exists("user", $_SESSION) && key_exists("EmployeeID", $_SESSION["user"])){
             if($GLOBALS["config"]["db_type"] == "mysql")
@@ -109,6 +113,7 @@ class DB{
         }
         return $GLOBALS["DB"]::insert($query, $args ? $args : array());
     }
+    
     public static function delete($query, $args = false){
         if(key_exists("user", $_SESSION) && key_exists("EmployeeID", $_SESSION["user"])){
             if($GLOBALS["config"]["db_type"] == "mysql")
@@ -117,6 +122,7 @@ class DB{
         }
         return $GLOBALS["DB"]::delete($query, $args ? $args : array());
     }
+    
     public static function connection($query = false, $args = false){
         if(key_exists("user", $_SESSION) && key_exists("EmployeeID", $_SESSION["user"])){
             if($GLOBALS["config"]["db_type"] == "mysql")
@@ -124,11 +130,69 @@ class DB{
         }
         return $GLOBALS["DB"]::connection();
     }
+    
     public static function getDatabaseName(){
         if(key_exists("user", $_SESSION) && key_exists("EmployeeID", $_SESSION["user"]))
             if($GLOBALS["config"]["db_type"] == "mysql")
                 $GLOBALS["DB"]::statement("set @EmployeeID='{$_SESSION["user"]["EmployeeID"]}'");
         return $GLOBALS["DB"]::getDatabaseName();
+    }
+    
+    public static function getFuncOrProcDefinition($name){
+        $names = [$name, $name . "1", $name . "2"];
+        $dbName = strtolower(DB::getDatabaseName());
+        $fname;
+        echo $dbName;
+        foreach($names as $name){
+            $routineDef = DB::select("SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME=? AND ROUTINE_SCHEMA=?", [$name, $dbName]);
+            if(count($routineDef)){
+                $routineDef = $routineDef[0];
+                $fname = $name;
+                break;
+            }                  
+        }
+
+        if($routineDef != null){
+            $params = DB::select("SELECT * FROM INFORMATION_SCHEMA.PARAMETERS  WHERE SPECIFIC_SCHEMA=? AND SPECIFIC_NAME=?  ORDER BY ORDINAL_POSITION", [$dbName, $fname]);
+            return [
+                "def" => $routineDef,
+                "params" => $params
+            ];
+        }else
+            return null;
+    }
+
+    public static function callProcedureOrFunction($name, $parameters){
+        $paramsDef = DB::getFuncOrProcDefinition($name);
+        if($paramsDef == null){
+            echo "Procedure or Function doesn't exists";
+            return null;
+        }
+        echo json_encode($paramsDef, JSON_PRETTY_PRINT);
+        if($GLOBALS["config"]["db_type"] == "mysql"){
+        }else if($GLOBALS["config"]["db_type"] == "sqlsrv"){
+        }
+        return;
+        if($GLOBALS["config"]["db_type"] == "mysql"){
+            DB::statement("CALL GetNextEntityID2(?, ?, ?, ?, @nextNumber, @ret)", [$user["CompanyID"], $user["DivisionID"], $user["DepartmentID"], $tablesForGetNextEntity[$this->tableName]]);
+            $columnMax = DB::select("select @nextNumber as nextNumber")[0]->nextNumber;
+        }else {
+            $tmpTableName = "GetNextEntityIDResult";
+            DB::statement("DROP TABLE IF EXISTS $tmpTableName");
+            DB::statement("CREATE TABLE $tmpTableName ( nextNumber NVARCHAR(36) )");
+            DB::statement("DECLARE @nextNumber NVARCHAR(36);EXEC Enterprise.GetNextEntityID ?, ?, ?, ?, @nextNumber OUTPUT; insert into $tmpTableName (nextNumber) values (@nextNumber)", ['DINOS', 'DEFAULT', 'DEFAULT', 'NextOrderNumber']);
+            $columnMax = DB::select("select * from $tmpTableName")[0]->nextNumber;
+        }
+
+        DB::statement("CALL Order_Post2('" . $user["CompanyID"] . "','" . $user["DivisionID"] . "','" . $user["DepartmentID"] . "','" . $_POST["OrderNumber"] . "',@PostingResult,@SWP_RET_VALUE)");
+
+        $result = DB::select('select @PostingResult as PostingResult, @SWP_RET_VALUE as SWP_RET_VALUE');
+        if($result[0]->SWP_RET_VALUE == -1) {
+            http_response_code(400);
+            echo $result[0]->PostingResult;
+        } else {
+            echo "ok";
+        }
     }
 }
 
